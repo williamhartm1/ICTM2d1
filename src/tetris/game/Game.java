a206469ce6769c616cfb8426cb2d62d231e67eb8
Jort Kuhlmann <jort@loylap.com>
2020-04-23 10:55:57 +0200

    game

--- src/tetris/game/Game.java
+++ src/tetris/game/Game.java
@@ -3,17 +3,41 @@ package tetris.game;
 public class Game {
     private final Board board;
 
-    private boolean playing = false;
+    private boolean isPlaying = false;
+    private boolean isDropping = false;
 
     public Game() {
         board = new Board();
     }
 
     public boolean isPlaying() {
-        return playing;
+        return isPlaying;
+    }
+
+    public boolean isDropping() {
+        return isDropping;
     }
 
     public void startGame() {
-        this.playing = true;
+        board.setCurrentBlock(Block.getRandomBlock());
+        this.isPlaying = true;
+    }
+
+    //  dropping delay
+    public long getIterationDelay() {
+        return (long) ((11 * 0.05) * 1000);
+    }
+
+    public BoardCell[][] getBoardCells() {
+        return board.getBoardWithPiece();
+    }
+
+    public void moveDown() {
+        if (!board.canCurrentPieceMoveDown()) {
+            isDropping = false;
+            board.setCurrentBlock(Block.getRandomBlock());
+        } else {
+            board.moveDown();
+        }
     }
 }
