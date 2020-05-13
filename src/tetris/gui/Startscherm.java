8e7163d5fd774933026a501a4c83427027554fad
JeMoeder <williamhartm@hotmail.com>
2020-05-13 12:18:46 +0200

    radio buttons easy/medium/hard

--- src/tetris/gui/Startscherm.java
+++ src/tetris/gui/Startscherm.java
@@ -11,19 +11,34 @@ public class Startscherm extends JFrame implements ActionListener {
     Game game;
     JButton jbStart, jbRank;
     JTextField jtNaam;
+    JRadioButton jrEasy, jrMedium, jrHard;
+    ButtonGroup group;
+    boolean isEasy, isMedium, isHard;
 
     public Startscherm(Game game){
         this.game = game;
         setSize(320, 200);
         setLayout(new FlowLayout());
-        setTitle("Tetris menu");
+        setTitle("Tetris hoofdmenu");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        add(new JLabel("NAAM:"));
+        add(new JLabel("Naam:"));
 
         jtNaam = new JTextField(20);
         add(jtNaam);
+
+        jrEasy = new JRadioButton("Easy", true);
+        add(jrEasy);
+        jrMedium = new JRadioButton("Medium");
+        add(jrMedium);
+        jrHard = new JRadioButton("Hard");
+        add(jrHard);
+
+        group = new ButtonGroup();
+        group.add(jrEasy);
+        group.add(jrMedium);
+        group.add(jrHard);
 
         jbStart = new JButton("Start spel");
         add(jbStart);
@@ -32,8 +47,6 @@ public class Startscherm extends JFrame implements ActionListener {
         jbRank = new JButton("Ranking");
         add(jbRank);
         jbRank.addActionListener(this);
-
-
     }
 
     @Override
