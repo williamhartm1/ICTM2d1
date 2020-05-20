75b4bdff14fef992de81b244a2788c36937e9030
Heleen de Gaaij <s1146233@student.windesheim.nl>
2020-05-20 09:49:04 +0200

    clearlines

--- src/tetris/game/Board.java
+++ src/tetris/game/Board.java
@@ -134,48 +134,102 @@ public class Board {
         blockCenter = new Point(blockCenter.x + moveX, blockCenter.y + moveY);
     }
 
-    public void clearLine() {
-        System.out.println("ik ben de clearline functie");
-        BoardCell[][] newBoard = createEmptyBoard();
-        BoardCell[] emptycel = BoardCell.getEmptyArray(1);
-        boolean clearable;
-        try {
-            int y;
-            for (y = 0; HEIGHT > y; y++) {
-                 clearable = true;
-                for (int x = 0; WIDTH > x; x++) {
-                    if (board[x][y].isEmpty()) {
-                        clearable = false;
-                    }
-                }
-                if (clearable) {
-                    System.out.println("ik ben clearable");
-                    for(int workline = y;workline > 0;workline--){
-                        int newline = y - 1;
-                        if(newline != -1) {
-                            for(int x = 0; x < WIDTH;x++)
-                            newBoard[x][newline] = newBoard[x][newline];
-                        }
-                        else{
-                            for(int x = 0;x < WIDTH;x++) {
-                                newBoard[x][workline] = board[x][workline];
-                            }
-                        }
-                    }
-                }
-                else{
-                    System.out.println("ik ben niet clearable");
-                    for(int m = 0;m < WIDTH;m++)
-                    newBoard[m][y] = board[m][y];
-                }
+    // Controleren of lijn vol is: isLineCompleted
+    // Complete lijnen verzamelen: collectCompletedLines (return array[])
+    // Nieuw bord vullen: fillNewBoard
+    //   initialiseer nieuw bord: createEmptyBoard
+    //   aanroepen collectCompletedLines
+    //   huidige y nieuw bord initialiseren: currentYnewBoard
+    //   loop door y van het oude bord
+    //      controleren of y in collectCompletedLines
+    //          als compleet: negeren
+    //          als niet compleet: overnemen in nieuw bord
+
+
+    public boolean isLineCompleted(int line){
+        for(int x = 0; x < WIDTH; x++){
+            if(board[x][line].isEmpty()){
+                return false;
             }
-            System.out.println("ik vervang het bord");
-            board = newBoard;
         }
-        catch(Exception ne){
-            ne.printStackTrace();
-            System.out.println("");
-            System.out.println(ne.toString());
+        return true;
+    }
+
+    public int[] collectCompletedLines(){
+        int[] completedLines = new int[4];
+        int aantal = 0;
+        for (int y =0; y < HEIGHT; y++){
+            if (isLineCompleted(y)){
+                completedLines[aantal] = y;
+                aantal++;
             }
+        }
+        return completedLines;
+    }
+
+    public void fillNewBoard(){
+        BoardCell[][] newBoard = createEmptyBoard();
+        int[] completedLines = collectCompletedLines();
+        int currentYnewBoard = 0;
+
+        for(int y = 0; y < HEIGHT; y++){
+            for (int i : completedLines) {
+                if (i != y) {
+                    newBoard[][]
+                }
+            }
+        }
+
+    }
+
+
+
+
+    public void clearLine() {
+
+//        System.out.println("ik ben de clearline functie");
+//        BoardCell[][] newBoard = createEmptyBoard();
+//        BoardCell[] emptycel = BoardCell.getEmptyArray(1);
+//        boolean clearable;
+//        try {
+//            int y;
+//            for (y = 0; HEIGHT > y; y++) {
+//                 clearable = true;
+//                for (int x = 0; WIDTH > x; x++) {
+//                    if (board[x][y].isEmpty()) {
+//                        clearable = false;
+//                    }
+//                }
+//                if (clearable) {
+//                    System.out.println("ik ben clearable");
+//                    for(int workline = y;workline > 0;workline--){
+//                        int newline = y + 1;
+//                        if(newline < 21) {
+//                            for(int x = 0; x < WIDTH;x++)
+//                            //klopt niet helemaal
+//                            newBoard[x][newline] = board[x][workline];
+//                        }
+//                        else{
+//                            for(int x = 0;x < WIDTH;x++) {
+//                            //klopt niet helemaal
+//                                newBoard[x][newline] = board[x][workline];
+//                            }
+//                        }
+//                    }
+//                }
+//                else{
+//                    System.out.println("ik ben niet clearable");
+//                    for(int m = 0;m < WIDTH;m++)
+//                    newBoard[m][y] = board[m][y];
+//                }
+//            }
+//            System.out.println("ik vervang het bord");
+//            board = newBoard;
+//        }
+//        catch(Exception ne){
+//            ne.printStackTrace();
+//            System.out.println("");
+//            System.out.println(ne.toString());
+//            }
         }
     }
